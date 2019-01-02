090d44cfc74de082e129cd09ace15970e718769b
Kusal <shrstha.kusal@gmail.com>
2019-01-02 16:41:20 +0545

    runnable

---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+import styles from './styles'
+
+class Home extends React.Component {
+
+  render() {
+    return (
+      <View style={styles.mainContainer}>
+        <Text>Home</Text>
+      </View>
+    )
+  }
+}
+
+export default Home
